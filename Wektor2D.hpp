8cee2d71887a1c1915c7971d44ffea8e2da405b6
Jaku6M <j.mazurkiewicz13@wp.pl>
2023-10-30 01:31:04 +0100

    Wyrzucony main bo miala byc napisana tylko klasa!

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -36,18 +36,3 @@ struct Wektor2D
         double x;
         double y;
 };
-
-int main()
-{
-    Wektor2D v1{};           // Konstruktor domyślny, wektor o wsp. [0, 0]
-    v1.setX(1.);             // setter
-    v1.setY(1.);             // setter
-    double x1 = v1.getX();   // getter
-    double y1 = v1.getY();   // getter
-
-    Wektor2D v2{2., 2.}; // Konstruktor nadający współrzędne
-
-    Wektor2D sum = v1 + v2; // dodawanie wektorów
-
-    double prod = v1 * v2; // iloczyn skalarny
-}
